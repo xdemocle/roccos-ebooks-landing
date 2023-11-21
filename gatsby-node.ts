f7fff630c14ffdd1a23ad896ff6fb8aaf1d4197d
xdemocle <xdemocle@gmail.com>
2023-11-21 18:57:55 +0100

    initial release

---
+++ gatsby-node.ts
@@ -0,0 +1,9 @@
+// export const createSchemaCustomization = ({ actions }) => {
+//   const { createTypes } = actions;
+//   const typeDefs = `
+//     type AuthorJson implements Node {
+//       joinedAt: Date
+//     }
+//   `;
+//   createTypes(typeDefs);
+// };
